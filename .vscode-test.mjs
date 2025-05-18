ef203c3dce4d60840c4d1c4301357af90a37bd2b
Corwin Marsh <corwinm@users.noreply.github.com>
2025-05-17 19:49:02 -0700

    test: more cleanup

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -4,16 +4,16 @@ import { fileURLToPath } from "url";
 import fs from "fs";
 
 const dirName = path.dirname(fileURLToPath(import.meta.url));
+const testTempDir = path.join(dirName, "test-temp");
 
 // Ensure the test temp directory is created
-const testTempDir = path.join(dirName, "test-temp");
 if (!fs.existsSync(testTempDir)) {
   fs.mkdirSync(testTempDir);
 }
 
 export default defineConfig({
   files: "out/test/**/*.test.js",
-  workspaceFolder: path.join(dirName, "test-temp"),
+  workspaceFolder: testTempDir,
   mocha: {
     timeout: 10000,
   },
