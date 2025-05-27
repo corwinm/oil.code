4ab3959a27382c332b651645c16b7ee6d41963fe
Corwin Marsh <corwinm@users.noreply.github.com>
2025-05-27 11:49:30 -0700

    feat: Add help command

--- esbuild.js
+++ esbuild.js
@@ -1,56 +1,88 @@
 const esbuild = require("esbuild");
+const fs = require("fs").promises;
+const path = require("path");
 
-const production = process.argv.includes('--production');
-const watch = process.argv.includes('--watch');
+const production = process.argv.includes("--production");
+const watch = process.argv.includes("--watch");
+
+const textFileNamespace = "text-file";
+
+/**
+ * @type {import('esbuild').Plugin}
+ */
+const textFilePlugin = {
+  name: "text-file",
+  setup(build) {
+    build.onResolve({ filter: /\.lua$/ }, (args) => {
+      const resolvedPath = path.resolve(args.resolveDir, args.path);
+      return {
+        path: resolvedPath,
+        namespace: textFileNamespace,
+      };
+    });
+    build.onLoad(
+      { filter: /.*/, namespace: textFileNamespace },
+      async (args) => {
+        const contents = await fs.readFile(args.path, "utf8");
+        return {
+          contents,
+          watchFiles: [args.path],
+          loader: "text",
+        };
+      }
+    );
+  },
+};
 
 /**
  * @type {import('esbuild').Plugin}
  */
 const esbuildProblemMatcherPlugin = {
-	name: 'esbuild-problem-matcher',
+  name: "esbuild-problem-matcher",
 
-	setup(build) {
-		build.onStart(() => {
-			console.log('[watch] build started');
-		});
-		build.onEnd((result) => {
-			result.errors.forEach(({ text, location }) => {
-				console.error(`✘ [ERROR] ${text}`);
-				console.error(`    ${location.file}:${location.line}:${location.column}:`);
-			});
-			console.log('[watch] build finished');
-		});
-	},
+  setup(build) {
+    build.onStart(() => {
+      console.log("[watch] build started");
+    });
+    build.onEnd((result) => {
+      result.errors.forEach(({ text, location }) => {
+        console.error(`✘ [ERROR] ${text}`);
+        console.error(
+          `    ${location.file}:${location.line}:${location.column}:`
+        );
+      });
+      console.log("[watch] build finished");
+    });
+  },
 };
 
 async function main() {
-	const ctx = await esbuild.context({
-		entryPoints: [
-			'src/extension.ts'
-		],
-		bundle: true,
-		format: 'cjs',
-		minify: production,
-		sourcemap: !production,
-		sourcesContent: false,
-		platform: 'node',
-		outfile: 'dist/extension.js',
-		external: ['vscode'],
-		logLevel: 'silent',
-		plugins: [
-			/* add to the end of plugins array */
-			esbuildProblemMatcherPlugin,
-		],
-	});
-	if (watch) {
-		await ctx.watch();
-	} else {
-		await ctx.rebuild();
-		await ctx.dispose();
-	}
+  const ctx = await esbuild.context({
+    entryPoints: ["src/extension.ts"],
+    bundle: true,
+    format: "cjs",
+    minify: production,
+    sourcemap: !production,
+    sourcesContent: false,
+    platform: "node",
+    outfile: "dist/extension.js",
+    external: ["vscode"],
+    logLevel: "silent",
+    plugins: [
+      /* add to the end of plugins array */
+      esbuildProblemMatcherPlugin,
+      textFilePlugin,
+    ],
+  });
+  if (watch) {
+    await ctx.watch();
+  } else {
+    await ctx.rebuild();
+    await ctx.dispose();
+  }
 }
 
-main().catch(e => {
-	console.error(e);
-	process.exit(1);
+main().catch((e) => {
+  console.error(e);
+  process.exit(1);
 });
